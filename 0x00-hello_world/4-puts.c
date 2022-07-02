bdd6ed9b0ccdf710cd126b11a7462d3cfec16fec
CyntiClem <cynthiaekeh40@gmail.com>
2022-07-02 12:24:20 -0700

    eighth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: ALWAYS 0 (Success)
  */
 int main(void)
-{puts ("\"programming is like building a multilingual puzzle");
+{puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
